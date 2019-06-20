3a282dc7fe2e31af6cb586613cd5158563c437a6
Rajesh Joshi <joshirajesh448@gmail.com>
2019-06-20 22:40:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 from getpass import getpass
-import time
 
 try:
     opts = Options()
